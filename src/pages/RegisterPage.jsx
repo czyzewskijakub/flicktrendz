4ac696ecf2d6749a1d86a5d60cebb81a2f821ab7
JulianWoroniecki <236702@edu.p.lodz.pl>
2023-04-26 17:19:25 +0200

    Add routing and register page

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Register from '../components/Login/Register';
+
+const LoginPage = () => {
+  return (
+    <React.Fragment>
+      <Register></Register>
+    </React.Fragment>
+  )
+};
+
+export default LoginPage;
