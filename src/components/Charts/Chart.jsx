de65ee65dddabe940da890080c6caec5d4daa258
JulianWoroniecki <236702@edu.p.lodz.pl>
2023-05-18 22:52:58 +0200

    Modify charts

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -13,27 +13,23 @@ const Chart = (props) => {
 
   const fetchDataHandler = useCallback(async () => {
     setIsLoading(true);
-    try {
-      const response = await fetch(props.url);
-      if (!response.ok) {
-        throw new Error('Some went wrong!');
+      try {
+          const response = await fetch(props.url);
+          if (!response.ok) {
+              throw new Error('Some went wrong!');
+          }
+          const jsonData = await response.json();
+          const jsonDataY = jsonData[props.y];
+          const chartData = Object.keys(jsonDataY).map((key) => ({
+              x: key,
+              y: jsonDataY[key]
+          }));
+          // console.log("chartData: " + chartData.toString());
+          setFetchData(chartData);
+      } catch (error) {
+          console.log(JSON.stringify(error));
       }
-
-      const data = await response.json();
-      setFetchData(data);
-    } catch (error) {
-      console.log(JSON.stringify(error));
-    }
-    // axios
-    //   .get(props.url)
-    //   .then(function (response) {
-    //     console.log(response);
-    //     setFetchData(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
-    setIsLoading(false);
+      setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -43,49 +39,42 @@ const Chart = (props) => {
   let chart = <p>No data provided.</p>;
 
   if (fetchData.length > 0) {
-    // const category = fetchData.map((obj) => obj.Category);
-    // const values = fetchData.map((obj) => obj['Average Likes']);
-    // const ticks = Array.from(
-    //   { length: fetchData.length },
-    //   (value, index) => index
-    // );
-    chart = (
-      <VictoryChart
-          domainPadding={{x: 20, y:10}}
-          // domainPadding={{left: 20, right: 20}}
-          padding={{ left: 80, bottom: 105 }}
-      >
-        <VictoryAxis
-            label="Category name"
-            style={{
-            ticks: { stroke: 'grey', size: 5 },
-            tickLabels: {
-              fontSize: 10,
-              padding: 6,
-              angle: -30,
-              textAnchor: 'end',
-            },
-            axisLabel: {padding: 75},
-          }}
-          padding={{ top: 20, bottom: 60 }}
-        />
-        <VictoryAxis
-            dependentAxis
-            tickFormat={(y) => y}
-        />
-        <VictoryBar
-            style={{
-                data: {
-                    fill: ({ index }) => +index % 2 === 0  ? "#4a14a2" : "#1631a9",
-                    stroke: "#000000",
-                    fillOpacity: 0.7,
-                    strokeWidth: 2
-                }
-            }}
-            data={fetchData} x="Category" y={props.y}
-        />
-      </VictoryChart>
-    );
+      chart = (
+          <VictoryChart
+              domainPadding={{x: 20, y:10}}
+              padding={{ left: 80, bottom: 105 }}
+              scale={{y: "sqrt"}}
+          >
+              <VictoryAxis
+                  style={{
+                      ticks: { stroke: 'grey', size: 5 },
+                      tickLabels: {
+                          fontSize: 10,
+                          padding: 6,
+                          angle: -30,
+                          textAnchor: 'end',
+                      },
+                      axisLabel: {padding: 75},
+                  }}
+                  // padding={{ top: 20, bottom: 60 }}
+                  dependentAxis
+                  tickFormat={(tick) => `${tick}`}
+              />
+              <VictoryAxis
+              />
+              <VictoryBar
+                  style={{
+                      data: {
+                          fill: ({ index }) => +index % 2 === 0  ? "#4a14a2" : "#1631a9",
+                          stroke: "#000000",
+                          fillOpacity: 0.7,
+                          strokeWidth: 2
+                      }
+                  }}
+                  data={fetchData}
+              />
+          </VictoryChart>
+      );
   }
 
   if (isLoading) {
--- src/pages/TilesList.jsx
+++ src/pages/TilesList.jsx
@@ -2,31 +2,33 @@ import React from 'react';
 
 import classes from './TilesList.module.css';
 import Chart from '../components/Charts/Chart';
+import StatisticsForm from "../components/Charts/StatisticsForm";
 
 const TilesList = () => {
   return (
     <React.Fragment>
+      <StatisticsForm/>
       <div className={classes['grid-container']}>
         <Chart
-          url="http://localhost:5000/ai/statistics/likes"
-          y="Average Likes"
-          title="Average Likes"
+          url="http://localhost:5000/ai/statistics/describe"
+          y="VideoCommentCount"
+          title="Video comment count"
         />
         <Chart
-          url="http://localhost:5000/ai/statistics/dislikes"
-          y="Average Dislikes"
-          title="Average Dislikes"
+          url="http://localhost:5000/ai/statistics/describe"
+          y="channelCommentCount"
+          title="Channel comment count"
         />
         <Chart
-          url="http://localhost:5000/ai/statistics/views"
-          y="Average Views"
-          title="Average Views"
+          url="http://localhost:5000/ai/statistics/describe"
+          y="channelViewCount"
+          title="channel view count"
         />
-        <Chart
-          url="http://localhost:5000/ai/statistics/comments"
-          y="Average Comments"
-          title="Average Comments"
-        />
+        {/*<Chart*/}
+        {/*  url="http://localhost:5000/ai/statistics/comments"*/}
+        {/*  y="Average Comments"*/}
+        {/*  title="Average Comments"*/}
+        {/*/>*/}
       </div>
     </React.Fragment>
   );
